ac03e437496b837ae5beffd3bb6831eb7bfa4acb
xiaofei <alibaba22001@163.com>
2024-12-30 16:48:07 +0800

    update

--- app/utils/model.ts
+++ app/utils/model.ts
@@ -232,6 +232,8 @@ export function isModelNotavailableInServer(
   modelName: string,
   providerNames: string | string[],
 ): boolean {
+  return false;
+
   // Check DISABLE_GPT4 environment variable
   if (
     process.env.DISABLE_GPT4 === "1" &&
